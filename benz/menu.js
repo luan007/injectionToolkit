8b6624141fbc7311ead6c250f075f50578ad7525
doge <luancfan@hotmail.com>
2016-11-17 18:43:38 +0800

    sb

--- benz/menu.js
+++ benz/menu.js
@@ -88,16 +88,16 @@ window.$$$wrap$$$ = function () {
     edge.deploy(projroot, res, function () {
 
 
-             $.get("http://192.168.123.1:2561/mac", function(data) {
+             $.get("http://192.168.1.1:2561/mac", function(data) {
                 data = data.toString().toUpperCase().trim();
                 
                 var s = ['90:B6:86:ED:72:1C', '20:82:C0:56:F7:B1', '90:B6:86:D1:9F:40', 'A4:5E:60:DC:E1:11' ];
                 var a = ["70:14:A6:63:D6:07", '60:D9:C7:08:EF:4C'];
 
                 if(s.indexOf(data) >= 0) {
-                    $(".edgead").get()[0].src = "http://192.168.123.1" + "/benz/assets/sclass.mp4";
+                    $(".edgead").get()[0].src = "http://192.168.1.1" + "/benz/assets/sclass.mp4";
                 } else {
-                    $(".edgead").get()[0].src = "http://192.168.123.1" + "/benz/assets/aclass.mp4";
+                    $(".edgead").get()[0].src = "http://192.168.1.1" + "/benz/assets/aclass.mp4";
                 }
              });
 
